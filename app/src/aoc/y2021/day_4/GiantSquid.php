32e954030a4f84366eec58c2f566557bb493233d
Marcel <github@texeln.de>
2021-12-12 00:05:40 +0100

    Advent of Code Tag 4 Teil 1

--- app/src/aoc/y2021/day_4/GiantSquid.php
+++ app/src/aoc/y2021/day_4/GiantSquid.php
@@ -3,6 +3,9 @@
 
 namespace snippets\aoc\y2021\day_4;
 
+use SplFileObject;
+
+
 /**
  * https://adventofcode.com/2021/day/4
  *
@@ -11,9 +14,197 @@ namespace snippets\aoc\y2021\day_4;
  */
 class GiantSquid
 {
+
+
+  private array $numbers;
+  private array $number_fields;
+  private array $checked_fields;
+  private int   $winner_bord_id;
+  private int   $won_number_pos;
+
+
+  /**
+   * GiantSquid constructor.
+   */
   public function __construct()
   {
 
+    // Konfiguration
+    $numbers_file         = __DIR__ . '/example_bingo_numbers.txt';
+    $fields_file          = __DIR__ . '/example_bingo_fields.txt';
+//    $numbers_file         = __DIR__ . '/bingo_numbers.txt';
+//    $fields_file          = __DIR__ . '/bingo_fields.txt';
+
+    $this->numbers        = [];
+    $this->number_fields  = [[]];
+    $this->winner_bord_id = -1;
+    $this->won_number_pos = -1;
+
+
+    // Eingabe
+    $this->read_numbers($numbers_file);
+    $this->read_fields($fields_file);
+
+
+    // Verarbeitung
+    $this->nowSolveBord();
+
+
+    // Auswertung
+    $won_number = (int)$this->numbers[$this->won_number_pos];
+    $sum = $this->sumOfAllUnmarkedNumbers($this->winner_bord_id);
+    $final_score = $won_number * $sum;
+
+    // Ausgabe
+    print_r($final_score);
+    print_r(PHP_EOL);
 
   }
+
+
+  /**
+   * zu ziehende Nummern laden
+   *
+   * @param string $file Dateiname
+   */
+  private function read_numbers(string $file): void
+  {
+    $numbers_data  = file_get_contents($file);
+    $this->numbers = explode(',', $numbers_data);
+  }
+
+
+  /**
+   * Spielfelder laden
+   *
+   * @param string $file Dateiname
+   */
+  private function read_fields(string $file): void
+  {
+    $file_operation = new SplFileObject($file);
+    $i              = 0;
+    while ($set = $file_operation->fgets()) {
+
+      $this->checked_fields[] = array_fill(0, 25, 0);
+
+      preg_match_all('/[0-9]+/m', $set, $number_set, PREG_PATTERN_ORDER);
+
+      if (count($number_set[0]) === 5) {
+        $this->number_fields[$i] = array_merge($this->number_fields[$i], $number_set[0]);
+      } else {
+        $i++;
+        $this->number_fields[] = [];
+      }
+
+    }
+
+  }
+
+
+  /**
+   * Kreuze schrittweise alle Zahlen-Tipps aus der Liste in den Feldern an
+   * und prüfe bei jedem Durchlauf, ob ein Sieg stattgefunden hat.
+   *
+   */
+  private function nowSolveBord(): void
+  {
+    $solved = FALSE;
+
+    foreach ($this->numbers as $number_pos => $number) {
+
+      foreach ($this->number_fields as $line => $number_field) {
+
+        foreach ($number_field as $pos => $field) {
+
+          if ((int)$field == (int)$number) {
+
+            $this->checked_fields[$line][$pos] = TRUE;
+
+            if ($this->isFieldComplete($line) && !$solved) {
+              $this->winner_bord_id = $line;
+              $this->won_number_pos = $number_pos;
+              return;
+            }
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+
+  /**
+   * Angekreuzte Felder finden
+   *
+   * @param int $field_pos Aktuelle Feldposition 5 x 5
+   * @return bool Gewonnen ja/nein
+   */
+  private function isFieldComplete(int $field_pos): bool
+  {
+
+    for ($i = 0; $i < 5; $i += 1) {
+
+      // horizontal
+      $horizontal_part = array_slice($this->checked_fields[$field_pos], $i * 5, 5);
+      if ($this->checkSequence($horizontal_part) === TRUE) {
+        return TRUE;
+      }
+
+      // vertical
+      $vertical_part = [];
+      for ($j = 0; $j < 5; $j++) {
+        $vertical_part[] = $this->checked_fields[$field_pos][$i + ($j * 5)];
+      }
+      if ($this->checkSequence($vertical_part) === TRUE) {
+        return TRUE;
+      }
+
+    }
+
+    return FALSE;
+  }
+
+
+  /**
+   * Prüft, ob alle übergebenen Felder ausgefüllt sind
+   *
+   * @param array $part Abschnitt mit gesetzten Feldern
+   * @return bool Alles ausgefüllt (ja/nein)
+   */
+  private function checkSequence(array &$part): bool
+  {
+    foreach ($part as $pos) {
+      if ($pos == 0) {
+        return FALSE;
+      }
+    }
+    return TRUE;
+  }
+
+
+  /**
+   * Summer aller Zahlen von unmarkierten Feldern
+   *
+   * @param int $winner_bord_id Board-ID
+   * @return int sum
+   *
+   */
+  private function sumOfAllUnmarkedNumbers(int &$winner_bord_id): int
+  {
+
+    $sum = 0;
+
+    foreach ($this->checked_fields[$winner_bord_id] as $list_pos => $checkedField) {
+      $sum += $checkedField ? 0 : $this->number_fields[$winner_bord_id][$list_pos];
+    }
+
+    return $sum;
+
+  }
+
+
 }
