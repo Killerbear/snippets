0384cbde6cd462bde1b39e71d3e53f346d36864b
Marcel <github@texeln.de>
2021-12-15 02:13:06 +0100

    Advent Of Code : Tag 7 / Teil 2

--- app/src/advent_of_code/y2021/day_7/TheTreacheryOfWhales.php
+++ app/src/advent_of_code/y2021/day_7/TheTreacheryOfWhales.php
@@ -13,28 +13,50 @@ class TheTreacheryOfWhales
   {
 
     // Konfiguration
-    $this->pos_data = [];
+    $this->pos_data   = [];
 
 
     // input
+//    $file = __DIR__ . '/example_input.txt';
     $file = __DIR__ . '/input.txt';
     $this->load_positions($file);
 
 
     // Median
     sort($this->pos_data);
-    $count      = count($this->pos_data);
-    $center_pos = intdiv($count, 2);
-    $median     = $this->pos_data[$center_pos];
-
+    $count  = count($this->pos_data);
+    $median = $this->pos_data[intdiv($count, 2)];
 
     $fuel = 0;
     foreach ($this->pos_data as $val) {
       $fuel += abs($median - $val);
     }
 
+
+    $max = max($this->pos_data);
+    $min = min($this->pos_data);
+
+    $fuel_gauss = array_fill(0, $max, 0);
+
+    for ($i = $min; $i < $max; $i++) {
+      foreach ($this->pos_data as $pos) {
+        $n              = abs($pos - $i);
+        $fuel_gauss[$i] += intdiv(($n * $n) + $n, 2);
+      }
+    }
+
+    $min = min($fuel_gauss);
+
+
+    // Ausgabe Teil 1
     print_r($fuel);
     print_r(PHP_EOL);
+
+    // Ausgabe Teil 2
+    print_r($min);
+    print_r(PHP_EOL);
+
+
   }
 
   /**
